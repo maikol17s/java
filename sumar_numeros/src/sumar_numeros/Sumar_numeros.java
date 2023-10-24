b05db0d6fe8e65d545c63a7895c21b315314e93e
maikol17s <maikol8382007@gmail.com>
2023-10-24 16:58:42 -0500

    Interfas

---
+++ sumar_numeros/src/sumar_numeros/Sumar_numeros.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sumar_numeros;
+
+/**
+ *
+ * @author SENA
+ */
+public class Sumar_numeros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
